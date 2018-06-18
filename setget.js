0a4af539791dfc91325ddeb46db08e3cc1bc4f5b
moryw <matt@skitskat.xyz>
2018-06-18 15:27:21 +0700

    Add setget file to play with setter and getters.

---
+++ setget.js
@@ -0,0 +1,32 @@
+const person = {
+  _name: 'Matt',
+  _age: 29,
+  _yearsBaptized: 15,
+  _yearsMarried: 1.8,
+
+  get name() {
+    console.log('This is using getter.');
+    return this._name;
+  },
+
+  set age(newAge) {
+    if (typeof newAge === 'number' && newAge !== this._age) {
+      this._age = newAge;
+      console.log(`Age has been updated.`);
+    } else {
+      console.log('Invalid input');
+      return 'Invalid input';
+    };
+  },
+
+  get age() {
+    return this._age;
+  }
+
+};
+
+console.log(person.age);
+person.age = 30;
+console.log(person.age);
+person._age = "thirty";
+console.log(person.age);
--- team.js
+++ team.js
@@ -1,3 +1,33 @@
 const team = {
-  
+  _players: [
+    {firstName: "Wayne",  lastName: "Gretzky",  age: 39},
+    {firstName: "Pavel",  lastName: "Bure",     age: 35},
+    {firstName: "Sidney", lastName: "Crosby",   age: 25}
+  ],
+  _games: [
+    {opponent: "Oilers", teamPoints: 3, opponentPoints: 1},
+    {opponent: "Flames", teamPoints: 2, opponentPoints: 3},
+    {opponent: "Penguins", teamPoints: 1, opponentPoints: 0}
+  ],
+
+  get players() {
+    return this._players;
+  },
+
+  get games() {
+    return this._games;
+  },
+
+  addPlayer: (firstName,lastName,age) => {
+
+  }
+
 };
+
+
+console.log(`The players are: ${team.players}`);
+console.log(`The games are: ${team.games}`);
+
+if (true) {
+
+}
